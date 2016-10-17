32a80ee8e946a52b991ada2fec626f25505aba7c
Razthgol <lucasmalvaso@gmail.com>
2016-10-17 12:45:59 -0300

    Basic player movement

---
+++ Tailon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float velocity;
+	public float rotation;
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () { 
+
+		if (Input.GetAxis ("Horizontal") < 0) {
+			gameObject.transform.Translate (Vector3.left * velocity);
+		} 
+		if (Input.GetAxis ("Horizontal") > 0){
+			gameObject.transform.Translate (Vector3.right * velocity);	
+		}
+
+		if (Input.GetAxis("Vertical")> 0) {
+			gameObject.transform.Translate (Vector3.forward * velocity);
+		} 
+
+		if (Input.GetAxis ("Vertical") < 0){
+			gameObject.transform.Translate (Vector3.back * velocity);	
+		}
+	}
+}
