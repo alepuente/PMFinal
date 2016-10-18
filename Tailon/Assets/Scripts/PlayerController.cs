069e3b510a71bb12405948723aa6bbfa81bce581
alepuente <ale3puente@gmail.com>
2016-10-18 10:09:27 -0300

    Player jump, movement

--- Tailon/Assets/Scripts/PlayerController.cs
+++ Tailon/Assets/Scripts/PlayerController.cs
@@ -1,30 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	public float velocity;
-	public float rotation;
-	void Start () {
+    public float _velocity;
+    public float _jump;
+    private Rigidbody rgb;
+    void Start()
+    {
+        rgb = gameObject.GetComponent<Rigidbody>();
+    }
 
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+        jump();
+    }
 
-	// Update is called once per frame
-	void Update () { 
+    void jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rgb.AddForce(0, _jump, 0);
+        }
+    }
+    void movement()
+    {
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            gameObject.transform.Translate(Vector3.left * _velocity);
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            gameObject.transform.Translate(Vector3.right * _velocity);
+        }
 
-		if (Input.GetAxis ("Horizontal") < 0) {
-			gameObject.transform.Translate (Vector3.left * velocity);
-		} 
-		if (Input.GetAxis ("Horizontal") > 0){
-			gameObject.transform.Translate (Vector3.right * velocity);	
-		}
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            gameObject.transform.Translate(Vector3.forward * _velocity);
+        }
 
-		if (Input.GetAxis("Vertical")> 0) {
-			gameObject.transform.Translate (Vector3.forward * velocity);
-		} 
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            gameObject.transform.Translate(Vector3.back * _velocity);
+        }
+    }
 
-		if (Input.GetAxis ("Vertical") < 0){
-			gameObject.transform.Translate (Vector3.back * velocity);	
-		}
-	}
+    void movementRGB()
+    {
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            rgb.AddForce(Vector3.left * _velocity);
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+           rgb.AddForce(Vector3.right * _velocity);
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+           rgb.AddForce(Vector3.forward * _velocity);
+        }
+
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            rgb.AddForce(Vector3.back * _velocity);
+        }
+    }
+
 }
