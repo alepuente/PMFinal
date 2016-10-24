7fa91b2d6f163cf79a4e06779ef552e426a18681
Razthgol <lucasmalvaso@gmail.com>
2016-10-24 13:04:16 -0300

    MeleeAttack Complete

--- Tailon/Assets/Scripts/MeleeAttackController.cs
+++ Tailon/Assets/Scripts/MeleeAttackController.cs
@@ -4,42 +4,37 @@ using System.Collections;
 public class MeleeAttackController : MonoBehaviour {
 
     PlayerController _playerController;
-    public Vector3 _distance;
-    public int _range;
-    public bool _hit;
-    public float _knockback = 1;
+	public float _range;
+    public float _knockback;
+    public float _distance;
 	// Use this for initialization
 	void Start () {
         _playerController = GetComponentInParent<PlayerController>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (_playerController._melee)
-        {
-            attack();
-        }
-	}
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(gameObject.transform.position+(gameObject.transform.forward*_distance), _range);
+    }
 
+    void Update()
+    {
+        attack();
+    }
     void attack()
     {        
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonUp(0))
         {
-            RaycastHit hit;
-            if (Physics.SphereCast(_playerController.transform.position + _distance, _range, transform.forward, out hit))
-            {
+            RaycastHit[] hit;
+            hit = Physics.SphereCastAll(gameObject.transform.position + (gameObject.transform.forward * _distance), _range, Vector3.forward);
+            
                 Debug.Log("Hit");
-                if (hit.collider.gameObject.tag == "Enemy")
-                {
-                    Debug.Log("Hit Enemy");
-                    hit.rigidbody.AddForce(Vector3.back);
-                }  
-                
-            }
-            else
-            {
-                Debug.Log("No hit");
-            }
+				for (int i = 0; i < hit.Length; i++) {				
+					if (hit[i].collider.gameObject.tag == "Enemy") {
+                    Debug.Log("Cast: "+gameObject.transform.position);
+                    hit[i].rigidbody.AddExplosionForce(_knockback, gameObject.transform.position + (gameObject.transform.forward * _distance), _range);
+					}       
+				}        
         }
     }
 }
