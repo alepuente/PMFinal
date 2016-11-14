2bcb2954b722c5c34ba75ee9c142c679488f2805
alepuente <ale3puente@gmail.com>
2016-11-13 23:58:15 -0300

    Menu

---
+++ Tailon/Assets/Tailon/Scripts/MenuController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+    public GameObject instructionsPanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void showInstructions()
+    {
+        if (instructionsPanel.activeSelf)
+        {
+            instructionsPanel.SetActive(false);
+        }
+        else
+        {
+            instructionsPanel.SetActive(true);
+        }
+    }
+
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+
+    public void toLobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+}
