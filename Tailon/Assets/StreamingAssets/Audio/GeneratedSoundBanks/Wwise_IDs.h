861d6450e5430321a5e2a60b7bd104db7346934f
AlePuente <alex_liga@hotmail.com>
2016-11-23 11:07:30 -0300

    Wwise Integration

---
+++ Tailon/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,57 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID DASH = 1942692385U;
+        static const AkUniqueID HITFLOOR = 2124419314U;
+        static const AkUniqueID INICIO = 2658298240U;
+        static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID MELEEHIT = 2087432578U;
+        static const AkUniqueID OBJECTIVE = 6899006U;
+        static const AkUniqueID PERDER = 2433235219U;
+        static const AkUniqueID PLAY = 1256202815U;
+        static const AkUniqueID SHOOT = 3038207054U;
+        static const AkUniqueID STARTBUTTON = 1651515387U;
+        static const AkUniqueID STEPPLAYER = 3003849848U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID NEW_SOUNDBANK = 4072029455U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
