e786643da0386af7524f1a6c0b4aa8834caddb7d
Maldorami <ramamaldonado@gmail.com>
2017-02-26 23:09:37 -0300

    Commit anterior pero "All"

---
+++ Tailon/Assets/Tailon/Bosses/Volcan/Scripts/SpawnPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SpawnPlayer : MonoBehaviour {
+
+    public GameObject Player;
+    public GameObject MainCamera;
+    public Canvas Canvas;
+
+	void Start () {
+        Instantiate(Player, transform.position, transform.rotation);
+        Instantiate(MainCamera, transform.position, transform.rotation);
+        Instantiate(Canvas, Vector3.zero, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
