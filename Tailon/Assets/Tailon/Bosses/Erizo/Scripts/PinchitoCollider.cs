4687c9bbf3669c639155c52b3fc888b69c9d0af2
Maldorami <ramamaldonado@gmail.com>
2017-02-27 23:22:31 -0300

    Segundo Boss -> a terminar

---
+++ Tailon/Assets/Tailon/Bosses/Erizo/Scripts/PinchitoCollider.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinchitoCollider : MonoBehaviour
+{
+
+    public int damage = 50;
+    public int speed = 50;
+
+    private Rigidbody rgb;
+    public float LifeTime;
+    private float tmp = 0;
+
+
+    void Start()
+    {
+
+        rgb = gameObject.GetComponent<Rigidbody>();
+
+    }
+
+    void OnTriggerEnter(Collider hit)
+    {
+        if (hit.gameObject.tag == "Player")
+        {
+            hit.gameObject.GetComponent<PlayerController>()._health -= damage;
+            Destroy(gameObject);
+        }
+
+    }
+
+    void Update()
+    {
+        rgb.AddForce(transform.forward * speed);
+
+        tmp += Time.deltaTime;
+        if (tmp >= LifeTime)
+            Destroy(gameObject);
+    }
+}
